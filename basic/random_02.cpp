ac123dbe4d42cad63301d37b8471aa7f61fcda74
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-29 18:42:17 +0300

    Update random_02.cpp

--- basic/random_02.cpp
+++ basic/random_02.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <chrono>
 
-using namespace std;
-
 int main()
 {
-	//mt19937 eng;
+	using namespace std;
+	
+	mt19937 eng;
+	//mt19937 eng{ mt19937::default_seed };
 	//mt19937 eng{ 34567u };
-	mt19937 eng{ static_cast<unsigned int>(chrono::steady_clock::now().time_since_epoch().count()) };
+	//mt19937 eng{ static_cast<unsigned int>(chrono::steady_clock::now().time_since_epoch().count()) };
 
 	for (int i = 0; i < 5; ++i)
 		cout << eng() << '\n';
