5cc2c3c2d97ca5fa9e029b1e6cda2c7ba278a483
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-29 18:31:16 +0300

    Update random_01.cpp

--- basic/random_01.cpp
+++ basic/random_01.cpp
@@ -1,18 +1,18 @@
 #include <random>
 #include <iostream>
 
-
 int main()
 {
 	using namespace std;
-	
+
 	mt19937 eng;
+	constexpr size_t n = 20;
 
 	cout << "min value    = " << mt19937::min() << '\n';
 	cout << "max value    = " << mt19937::max() << '\n';
 	cout << "default seed = " << mt19937::default_seed << '\n';
-	cout << "20 random numbers are being generated:\n";
+	cout << n << " random numbers are being generated:\n";
 
-	for (int i = 0; i < 20; ++i)
+	for (int i = 0; i < n; ++i)
 		cout << eng() << '\n';
 }
