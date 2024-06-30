419b67d702ee48958afb2a889d3ff6126c854ebb
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-30 07:12:19 +0300

    Update param_01.cpp

--- distribution/param_01.cpp
+++ distribution/param_01.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
 #include <random>
 
 int main()
 {
-	std::uniform_int_distribution dist_1{ 3456, 987 };
+	std::uniform_int_distribution dist_1{987, 4567 };
 	//...
 	std::uniform_int_distribution dist_2{dist_1.param()};
 	//...
-
 }
