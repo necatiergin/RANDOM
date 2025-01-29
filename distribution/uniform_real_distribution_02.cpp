cb5530591eb715e106cfe4abab1eefec9ffb9261
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-29 18:18:43 +0300

    Update uniform_real_distribution_02.cpp

--- distribution/uniform_real_distribution_02.cpp
+++ distribution/uniform_real_distribution_02.cpp
@@ -3,10 +3,7 @@
 
 int main()
 {
-	using namespace std;
-
-	uniform_real_distribution dist{ 18.45, 67.98 };
-
-	cout << "a value is : " << dist.a() << '\n';
-	cout << "b value is : " << dist.b() << '\n';
+	std::uniform_real_distribution dist{ 18.45, 67.98 };
+	std::cout << "a value is : " << dist.a() << '\n';
+	std::cout << "b value is : " << dist.b() << '\n';
 }
